aff4256b5dccedbb448a13fa9c017421a1985c40
uttamYP5697 <uttam@yellowpanther.co.uk>
2025-09-14 14:29:15 +0530

    fixed merge

--- src/components/StripeProviderWrapper.tsx
+++ src/components/StripeProviderWrapper.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe("pk_test_Xqm1R4ZduOt03cOvIg3vuozX00MZteqamV");
+const stripePromise = loadStripe("pk_live_2kRI3s0orAINwm7WST6rG09a00iRuvqEVl");
 
 export default function StripeProviderWrapper({ children }: { children: React.ReactNode }) {
   return <Elements stripe={stripePromise}>{children}</Elements>;
